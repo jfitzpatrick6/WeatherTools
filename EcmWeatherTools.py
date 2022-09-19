95a35921d12c800538340db46af8f31789ec8359
jfitzpatrick6 <112958802+jfitzpatrick6@users.noreply.github.com>
2022-09-19 16:29:30 -0400

    Initial Commit

---
+++ EcmWeatherTools.py
@@ -0,0 +1,30 @@
+## Name of file: EcmWeatherTools.py
+## Description: functions to grab often used weather data points
+## Last Edited: 9/19/2022
+## Last Edited By: jfitzpatrick
+## Time in Development: 1 hour
+
+# GOALS
+
+# 7 day forecast based on zipcode https://api.weather.gov/gridpoints/BOX/71,90
+# avg hi/low this week
+# avg humidity this week
+# projected wind speeds/ direction
+# solar radiation for solar power projections https://openweathermap.org/api/solar-radiation
+# solar projections?
+# wind projections
+
+def weekforecast(zipcode):
+    return True
+
+def solarRadiation(zipcode):
+    return True
+
+def windForecast(zipcode):
+    return True
+
+def humidityForecast(zipcode):
+    return True
+
+def tolatlong(zipcode):
+    return True
